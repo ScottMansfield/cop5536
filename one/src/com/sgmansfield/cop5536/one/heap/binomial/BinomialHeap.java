279b37893d5e689f41596422877535a3fecc3c32
Scott Mansfield <sgmansf@gmail.com>
2013-06-25 23:53:47 -0700

    BinomialHeap insert done. deleteMin partially done with notes on the rest of the implementation.

--- one/src/com/sgmansfield/cop5536/one/heap/binomial/BinomialHeap.java
+++ one/src/com/sgmansfield/cop5536/one/heap/binomial/BinomialHeap.java
@@ -1,6 +1,8 @@
 package com.sgmansfield.cop5536.one.heap.binomial;
 
 import com.sgmansfield.cop5536.one.heap.Heap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -8,33 +10,61 @@ import com.sgmansfield.cop5536.one.heap.Heap;
  */
 public class BinomialHeap implements Heap
 {
-    //private Node root = null;
-    
-    public BinomialHeap()
-    {
-        // initialize heap
-    }
+    private BinomialNode root = null;
     
     @Override
     public void insert(int num)
     {
-        System.out.println("Inserting " + num);
+        BinomialNode newNode = new BinomialNode(num, 0, null, null);
+        
+        if (root == null)
+        {
+            root = newNode;
+            root.setNext(root);
+        }
+        else
+        {
+            newNode.setNext(root.getNext());
+            root.setNext(newNode);
+        }
     }
 
     @Override
     public Integer deleteMin()
     {
-        System.out.println("Deleting min");
-        return 0;
-    }
-    
-    /**
-     * Merges this heap with another BinomialHeap.
-     * 
-     * @param otherHeap 
-     */
-    public void meld(BinomialHeap otherHeap)
-    {
+        if (root == null)
+        {
+            return -1;
+        }
         
+        int retval = root.getData();
+        
+        if (root == root.getNext())
+        {
+            // delete the root node!
+            root = null;
+            return retval;
+        }
+        
+        Map<Integer, BinomialNode> nodes = new HashMap<>();
+        
+        // merge trees here. Make sure to keep a reference to the original node
+        // so there's no infinite loop
+        BinomialNode originalNode = root;
+        
+        BinomialNode currentNode = root.getNext();
+        
+        // run through the list and merge all of the possible trees together
+        while (currentNode != originalNode)
+        {
+            // do stuff
+            // need a while loop here to say while the nodes map doesn't have
+            //   an entry, merge trees
+        }
+        
+        // iterate over the hashmap and serialize to a list
+        //   while maintaining a min to use as the root element.
+        
+        return retval;
     }
 }
