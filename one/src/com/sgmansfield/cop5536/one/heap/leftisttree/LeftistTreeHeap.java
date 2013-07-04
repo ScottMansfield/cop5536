6c71f6b7faf77bea5dd991a28c16d21dca487c23
Scott Mansfield <sgmansf@gmail.com>
2013-07-04 10:33:58 -0700

    LeftistTreeHeap is actually a min heap now and has a toString to print itself

--- one/src/com/sgmansfield/cop5536/one/heap/leftisttree/LeftistTreeHeap.java
+++ one/src/com/sgmansfield/cop5536/one/heap/leftisttree/LeftistTreeHeap.java
@@ -1,6 +1,8 @@
 package com.sgmansfield.cop5536.one.heap.leftisttree;
 
 import com.sgmansfield.cop5536.one.heap.Heap;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  *
@@ -9,7 +11,7 @@ import com.sgmansfield.cop5536.one.heap.Heap;
 public class LeftistTreeHeap implements Heap
 {
     private LeftistNode root;
-    
+
     @Override
     public void insert(int num)
     {
@@ -26,13 +28,13 @@ public class LeftistTreeHeap implements Heap
         {
             return null;
         }
-        
+
         // Meld both subtrees to get the new tree, then return the former root
         int retval = this.root.getData();
         this.root = meld(root.getLeft(), root.getRight());
         return retval;
     }
-    
+
     private static LeftistNode meld(LeftistNode node1, LeftistNode node2)
     {
         // First, take care of some base cases.
@@ -44,7 +46,7 @@ public class LeftistTreeHeap implements Heap
         {
             return null;
         }
-        
+
         // if one is null and the other is not, return the not null one.
         // this is the base case for the recursion.
         if (node1 != null &&
@@ -52,19 +54,19 @@ public class LeftistTreeHeap implements Heap
         {
             return node1;
         }
-        
+
         if (node1 == null &&
             node2 != null)
         {
             return node2;
         }
-        
+
         // now for the meld operation
         // at this point both nodes should be not null
         LeftistNode master = null;
         LeftistNode slave = null;
-        
-        if (node1.getData() > node2.getData())
+
+        if (node1.getData() < node2.getData())
         {
             master = node1;
             slave = node2;
@@ -74,7 +76,7 @@ public class LeftistTreeHeap implements Heap
             master = node2;
             slave = node1;
         }
-        
+
         // The right subtree of node1 is melded with all of node2 and the
         //   result is reattached as the right subtree of node 1
         master.setRight(meld(master.getRight(), slave));
@@ -90,7 +92,7 @@ public class LeftistTreeHeap implements Heap
             master.setLeft(master.getRight());
             master.setRight(temp);
         }
-        
+
         // Second, make sure the right distance is correct
         if (master.getRight() == null)
         {
@@ -100,7 +102,59 @@ public class LeftistTreeHeap implements Heap
         {
             master.setRightDistance(master.getRight().getRightDistance() + 1);
         }
-        
+
         return master;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        LeftistNode sentinel = new LeftistNode(-1, null, null, -1);
+        int level = 1;
+
+        Queue<LeftistNode> q = new LinkedBlockingQueue<>();
+        LeftistNode currentNode = null;
+        boolean first = true;
+
+        q.add(root);
+        q.add(sentinel);
+
+        while (!(q.peek() == sentinel))
+        {
+            sb.append("Level ").append(level).append(": [");
+
+            first = true;
+
+            while ((currentNode = q.remove()) != sentinel)
+            {
+                if (!first)
+                {
+                    sb.append(", ");
+                }
+
+                first = false;
+
+                sb.append(currentNode.getData());
+
+                if (currentNode.getLeft() != null)
+                {
+                    q.add(currentNode.getLeft());
+                }
+
+                if (currentNode.getRight() != null)
+                {
+                    q.add(currentNode.getRight());
+                }
+            }
+
+            sb.append("]\n");
+
+            q.add(sentinel);
+            level++;
+        }
+
+        return sb.toString();
     }
 }
